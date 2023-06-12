cf98eebd794054bee2f44496e916291a74ccf93d
notebook <ljh1642@gmail.com>
2023-06-12 11:16:31 +0900

    aaa

--- 04_Object_Relationship/01_Object_Relationship.cpp
+++ 04_Object_Relationship/01_Object_Relationship.cpp
@@ -11,7 +11,11 @@
 
 	++ Inheritance(상속) is-a
 
-	//TODO 카톡내용 복사 필요
+	                                  관계 형태    다른 클래스에 속할수 있는가  멤버의 존재를 클래스가 관리  방향성
+   Composition(구성)     part-of     전체/부품                   No                        Yes             단방향
+   Aggregation(집합)     has-a       전체/부품                   Yes                       No              단방향
+   Association(연계)     uses-a      용도 외엔 무관              Yes                       No              단방향 or 양방향
+   Dependency(의존)      depends-on  옹도 외엔 무관              Yes                       Yes             단방향
 */
 
 /*
